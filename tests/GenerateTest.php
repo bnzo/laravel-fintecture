7a7fe54ec93e28973112d9232ee9e2db3768e91f
bnzo <17174973+bnzo@users.noreply.github.com>
2025-08-15 18:20:24 +0200

    Refactor GenerateTest to use FintectureTester for mocking responses

--- tests/GenerateTest.php
+++ tests/GenerateTest.php
@@ -1,33 +1,16 @@
 <?php
 
 use Bnzo\Fintecture\Fintecture;
-use Fintecture\PisClient;
-use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Psr7\Response;
+use Bnzo\Fintecture\Tests\FintectureTester;
 
 it('can generate', function () {
-
-    $mock = new MockHandler([
-        new Response(200, [], json_encode(['access_token' => 'mock_access_token'])),
-        new Response(200, [], json_encode(['meta' => [
+    FintectureTester::mockResponses([
+        ['access_token' => 'mock_access_token'],
+        ['meta' => [
             'url' => 'https://mock.url/fintecture',
             'session_id' => 'mock_session_id',
-        ]])),
+        ]],
     ]);
-
-    $handlerStack = HandlerStack::create($mock);
-    $client = new Client(['handler' => $handlerStack]);
-
-    app()->singleton(Fintecture::class, function ($app) use ($client) {
-        return new Fintecture(new PisClient([
-            'appId' => 'ok',
-            'appSecret' => 'ok',
-            'privateKey' => base64_decode(config('fintecture.private_key')),
-            'environment' => config('fintecture.environment'),
-        ], $client));
-    });
 
     $url = app(Fintecture::class)->generate();
 
