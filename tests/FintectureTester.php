152506d6d8c8e24fd4240e631a7e0780c04b6abe
bnzo <17174973+bnzo@users.noreply.github.com>
2025-08-15 19:09:53 +0200

    Refactor FintectureTester to use Http\Mock\Client for response mocking

--- tests/FintectureTester.php
+++ tests/FintectureTester.php
@@ -4,24 +4,26 @@ namespace Bnzo\Fintecture\Tests;
 
 use Bnzo\Fintecture\Fintecture;
 use Fintecture\PisClient;
-use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
-use Illuminate\Support\Arr;
+use Http\Mock\Client;
 
 class FintectureTester
 {
     public static function mockResponses(array $responses)
     {
-        $mock = new MockHandler(
-            Arr::map($responses, fn ($data) => new Response(200, [], json_encode($data)))
-        );
+        $client = new Client;
 
-        $handlerStack = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handlerStack]);
+        foreach ($responses as $data) {
+            $client->addResponse(
+                new Response(
+                    status: 200,
+                    headers: [],
+                    body: json_encode($data)
+                )
+            );
+        }
 
-        app()->singleton(Fintecture::class, function ($app) use ($client) {
+        app()->singleton(Fintecture::class, function () use ($client) {
             return new Fintecture(new PisClient([
                 'appId' => 'test',
                 'appSecret' => 'test',
