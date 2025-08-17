d7d95a3536529e6947134c4afcfeceb9269f6cd9
bnzo <17174973+bnzo@users.noreply.github.com>
2025-08-17 11:34:03 +0200

    Refactor GenerateTest to improve clarity and ensure exception handling for URL generation

--- tests/GenerateTest.php
+++ tests/GenerateTest.php
@@ -2,23 +2,39 @@
 
 use Bnzo\Fintecture\Facades\Fintecture;
 use Bnzo\Fintecture\Tests\FintectureTester;
+use Fintecture\Util\FintectureException;
 use GuzzleHttp\Psr7\Response;
 
-it('can generate', function () {
+it('can generate url', function () {
     FintectureTester::mockResponses([
         new Response(
-            body: json_encode(['access_token' => 'mock_access_token'])
-        ),
-        new Response(
-
             body: json_encode(['meta' => [
                 'url' => 'https://mock.url/fintecture',
                 'session_id' => 'mock_session_id',
-            ]], )
+            ]])
+        ),
+    ], );
+
+    $url = Fintecture::generate();
+
+    expect($url)->toBe('https://mock.url/fintecture');
+});
+
+it('can throw an exception generate url', function () {
+    FintectureTester::mockResponses([
+        new Response(
+            status: 400,
+            body: json_encode(['status' => '400', 'errors' => [
+                [
+                    'code' => 'mock_error_code',
+                    'title' => 'mock_error_title',
+                    'message' => 'mock_error_message',
+                ],
+            ]])
         ),
     ]);
 
     $url = Fintecture::generate();
 
     expect($url)->toBe('https://mock.url/fintecture');
-});
+})->throws(FintectureException::class, 'mock_error_message');
