eed0f81cc4af0c358ac79acc0dfb4f3372984185
bnzo <17174973+bnzo@users.noreply.github.com>
2025-08-26 16:41:27 +0200

    Refactor PaymentCustomerData constructor to set default values for optional parameters and improve readability; add eval statement in GenerateTest for debugging purposes

--- src/Data/PaymentCustomerData.php
+++ src/Data/PaymentCustomerData.php
@@ -10,12 +10,20 @@ class PaymentCustomerData extends Data
     public function __construct(
         public string $psu_email,
         public string $psu_name,
-        public string|Optional|null $psu_company,
-        public string|Optional|null $psu_form,
-        public string|Optional|null $psu_incorporation,
-        public string|Optional|null $psu_phone,
-        public string|Optional|null $psu_phone_prefix,
-        public string|Optional|null $psu_ip,
-        public AddressData|Optional|null $psu_address,
-    ) {}
+        public string|Optional|null $psu_company = null,
+        public string|Optional|null $psu_form = null,
+        public string|Optional|null $psu_incorporation = null,
+        public string|Optional|null $psu_phone = null,
+        public string|Optional|null $psu_phone_prefix = null,
+        public string|Optional|null $psu_ip = null,
+        public AddressData|Optional|null $psu_address = null,
+    ) {
+        $this->psu_company = $psu_company ?: Optional::create();
+        $this->psu_form = $psu_form ?: Optional::create();
+        $this->psu_incorporation = $psu_incorporation ?: Optional::create();
+        $this->psu_phone = $psu_phone ?: Optional::create();
+        $this->psu_phone_prefix = $psu_phone_prefix ?: Optional::create();
+        $this->psu_ip = $psu_ip ?: Optional::create();
+        $this->psu_address = $psu_address ?: Optional::create();
+    }
 }
