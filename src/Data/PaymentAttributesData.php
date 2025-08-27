1469cebdec3360b946166c98517a5c3a3ab6ae0d
bnzo <17174973+bnzo@users.noreply.github.com>
2025-08-27 14:03:49 +0200

    Refactor PaymentAttributesData to simplify communication property initialization

--- src/Data/PaymentAttributesData.php
+++ src/Data/PaymentAttributesData.php
@@ -6,16 +6,13 @@ use Bnzo\Fintecture\Enums\Currency;
 use Spatie\LaravelData\Attributes\WithCast;
 use Spatie\LaravelData\Casts\EnumCast;
 use Spatie\LaravelData\Data;
-use Spatie\LaravelData\Optional;
 
 class PaymentAttributesData extends Data
 {
     public function __construct(
         public string $amount,
         #[WithCast(EnumCast::class, Currency::class)]
+        public string $communication,
         public ?Currency $currency = Currency::EUR,
-        public string|Optional|null $communication = null,
-    ) {
-        $this->communication = $communication ?: Optional::create();
-    }
+    ) {}
 }
