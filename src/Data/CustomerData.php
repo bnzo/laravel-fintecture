85640cd847ed97f01001b2541db812df53b03667
bnzo <17174973+bnzo@users.noreply.github.com>
2025-09-01 23:12:54 +0200

    Refactor CustomerData class to use mapped input/output names for properties

--- src/Data/CustomerData.php
+++ src/Data/CustomerData.php
@@ -2,28 +2,48 @@
 
 namespace Bnzo\Fintecture\Data;
 
+use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Attributes\MapOutputName;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Optional;
 
 class CustomerData extends Data
 {
     public function __construct(
-        public string $psu_email,
-        public string $psu_name,
-        public string|Optional|null $psu_company = null,
-        public string|Optional|null $psu_form = null,
-        public string|Optional|null $psu_incorporation = null,
-        public string|Optional|null $psu_phone = null,
-        public string|Optional|null $psu_phone_prefix = null,
-        public string|Optional|null $psu_ip = null,
-        public AddressData|Optional|null $psu_address = null,
+        #[MapInputName('psu_email')]
+        #[MapOutputName('psu_email')]
+        public string $email,
+        #[MapInputName('psu_name')]
+        #[MapOutputName('psu_name')]
+        public string $name,
+        #[MapInputName('psu_company')]
+        #[MapOutputName('psu_company')]
+        public string|Optional|null $company = null,
+        #[MapInputName('psu_form')]
+        #[MapOutputName('psu_form')]
+        public string|Optional|null $form = null,
+        #[MapInputName('psu_incorporation')]
+        #[MapOutputName('psu_incorporation')]
+        public string|Optional|null $incorporation = null,
+        #[MapInputName('psu_phone')]
+        #[MapOutputName('psu_phone')]
+        public string|Optional|null $phone = null,
+        #[MapInputName('psu_phone_prefix')]
+        #[MapOutputName('psu_phone_prefix')]
+        public string|Optional|null $phone_prefix = null,
+        #[MapInputName('psu_ip')]
+        #[MapOutputName('psu_ip')]
+        public string|Optional|null $ip = null,
+        #[MapInputName('psu_address')]
+        #[MapOutputName('psu_address')]
+        public AddressData|Optional|null $address = null,
     ) {
-        $this->psu_company = $psu_company ?: Optional::create();
-        $this->psu_form = $psu_form ?: Optional::create();
-        $this->psu_incorporation = $psu_incorporation ?: Optional::create();
-        $this->psu_phone = $psu_phone ?: Optional::create();
-        $this->psu_phone_prefix = $psu_phone_prefix ?: Optional::create();
-        $this->psu_ip = $psu_ip ?: Optional::create();
-        $this->psu_address = $psu_address ?: Optional::create();
+        $this->company = $company ?: Optional::create();
+        $this->form = $form ?: Optional::create();
+        $this->incorporation = $incorporation ?: Optional::create();
+        $this->phone = $phone ?: Optional::create();
+        $this->phone_prefix = $phone_prefix ?: Optional::create();
+        $this->ip = $ip ?: Optional::create();
+        $this->address = $address ?: Optional::create();
     }
 }
