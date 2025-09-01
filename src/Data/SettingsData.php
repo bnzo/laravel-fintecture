4340af3ca3c9e906fce791b921a6261f7b2fe3f8
bnzo <17174973+bnzo@users.noreply.github.com>
2025-09-01 23:30:27 +0200

    Reorder constructor parameters in SettingsData for clarity and consistency

--- src/Data/SettingsData.php
+++ src/Data/SettingsData.php
@@ -7,18 +7,21 @@ use Bnzo\Fintecture\Enums\ScheduledExpirationPolicy;
 use Spatie\LaravelData\Attributes\WithCast;
 use Spatie\LaravelData\Casts\EnumCast;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Optional;
 
 class SettingsData extends Data
 {
     public function __construct(
-        public int $expiry = 86400,
         public int $due_date = 86400,
-        public bool $permanent = false,
-        #[WithCast(EnumCast::class, ScheduledExpirationPolicy::class)]
-        public ScheduledExpirationPolicy $scheduled_expiration_policy = ScheduledExpirationPolicy::Immediate,
+        public int $expiry = 86400,
         #[WithCast(EnumCast::class, Method::class)]
         public Method $method = Method::Link,
+        public bool $permanent = false,
+        public string|Optional|null $redirectUri = null,
+        #[WithCast(EnumCast::class, ScheduledExpirationPolicy::class)]
+        public ScheduledExpirationPolicy $scheduled_expiration_policy = ScheduledExpirationPolicy::Immediate,
     ) {
         $this->permanent = $permanent ?: false;
+        $this->redirectUri = $redirectUri ?: Optional::create();
     }
 }
